e96d933038830785a0e4b211d71eb35ed80df508
TomQuintana <tomquintana20@gmail.com>
2022-10-16 15:54:17 -0300

    add routes files for start routing

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conectarDb from './config/db.js'
+import comidasRoutes from './routes/comidasRoutes.js'
 
 const app = express();
 dotenv.config();
 
 conectarDb();
 
-app.use('/', (req, res) => {
-  res.send('Hola Mundo')
-})
+app.use('/api/comidas', comidasRoutes)
 
 app.listen(4000, () => {
   console.log('Servidor corriendo en el puerto 4000')
