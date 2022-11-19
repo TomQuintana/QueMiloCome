015a942a1d560e6a3ffb12bc59d410c8399e9736
TomQuintana <tomquintana20@gmail.com>
2022-11-19 19:54:39 -0300

    add cors and configure cors config

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import conectarDb from './config/db.js'
 import comidasRoutes from './routes/comidasRoutes.js'
 
@@ -8,6 +9,23 @@ const app = express();
 app.use(express.json())
 
 dotenv.config();
+
+// permitir que me pegue x dominio
+const dominiosPermitidos = ['http://localhost:5173']
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(dominiosPermitidos.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido por CORS'))
+    }
+  }
+}
+
+
+app.use(cors(corsOptions))
+
 
 conectarDb();
 
